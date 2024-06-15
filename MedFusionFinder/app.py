c0c19544b6f86e6dad23cc18494733f9fe3577f2
JoannaRed <j.baranowska@hotmail.com>
2024-06-15 12:01:45 +0200

    add insertion inside ES

--- MedFusionFinder/app.py
+++ MedFusionFinder/app.py
@@ -3,6 +3,7 @@ import mysql.connector
 from mysql.connector import Error
 import pysftp
 from faker import Faker
+import pandas as pd
 from elasticsearch import Elasticsearch
 import os
 import sys
@@ -26,6 +27,10 @@ SFTP_HOST = os.getenv('SFTP_HOST', 'sftp')
 SFTP_PORT = int(os.getenv('SFTP_PORT', 22))
 SFTP_USERNAME = os.getenv('SFTP_USERNAME', 'sftpuser')
 SFTP_PASSWORD = os.getenv('SFTP_PASSWORD', 'password')
+
+# Initialize Elasticsearch client
+es = Elasticsearch([{'host': ES_HOST, 'port': ES_PORT}])
+
 
 # Disable host key checking for testing
 cnopts = pysftp.CnOpts()
@@ -74,6 +79,83 @@ def test_sftp_connection():
     except Exception as e:
         logging.error(f"Error testing SFTP connection: {str(e)}")
         return jsonify({"error": str(e)}), 500
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'files' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    files = request.files.getlist('files')
+
+    if not files or files[0].filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    upload_results = []
+
+    for file in files:
+        if file:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            logging.debug(f"Saving file to {file_path}")
+            try:
+                file.save(file_path)
+                logging.debug(f"File saved to {file_path}")
+                os.chmod(file_path, 0o777)
+                logging.debug(f"Permissions set for {file_path}")
+
+                with pysftp.Connection(SFTP_HOST, username=SFTP_USERNAME, password=SFTP_PASSWORD, port=SFTP_PORT, cnopts=cnopts) as sftp:
+                    sftp.put(file_path, f'upload/{file.filename}')
+                    logging.debug(f"File {file.filename} uploaded to SFTP server")
+                upload_results.append(f"File {file.filename} uploaded successfully")
+            except Exception as e:
+                logging.error(f"Error saving file {file.filename}: {str(e)}")
+                return jsonify({"error": str(e)}), 500
+            finally:
+                os.remove(file_path)
+                logging.debug(f"File {file.filename} removed from {file_path}")
+
+    return jsonify({"message": upload_results}), 200
+
+@app.route('/list_files', methods=['GET'])
+def list_files():
+    try:
+        with pysftp.Connection(SFTP_HOST, username=SFTP_USERNAME, password=SFTP_PASSWORD, port=SFTP_PORT, cnopts=cnopts) as sftp:
+            sftp.cwd('upload')
+            files = sftp.listdir()
+        return jsonify({"files": files}), 200
+    except Exception as e:
+        logging.error(f"Error listing files: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/process_csv', methods=['GET'])
+def process_csv():
+    try:
+        # Connect to SFTP and download the file
+        with pysftp.Connection(SFTP_HOST, username=SFTP_USERNAME, password=SFTP_PASSWORD, port=SFTP_PORT, cnopts=cnopts) as sftp:
+            sftp.cwd('upload')
+            csv_files = [f for f in sftp.listdir() if f.endswith('.csv')]
+            logging.debug(f"File {csv_files}")
+            if not csv_files:
+                return jsonify({"error": "No CSV files found"}), 400
+            sftp.get(csv_files[0], os.path.join(app.config['UPLOAD_FOLDER'], csv_files[0]))    
+
+            csv_file = os.path.join(app.config['UPLOAD_FOLDER'], csv_files[0])
+            logging.debug(f"2) File {csv_file}")
+            data = pd.read_csv(csv_file, delimiter=',')
+
+            for index, row in data.iterrows():
+                doc = {
+                    'PID:': row['PID'],
+                    'Pathology': row['Pathology']
+                }
+                res = es.index(index='medical_data', body=doc)
+                logging.debug(f"Inserted document ID: {res['_id']}")
+
+            return jsonify({"message": "Data processed and inserted successfully"}), 200
+    
+    except Exception as e:
+        logging.error(f"Error processing CSV: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/')
 def index():
